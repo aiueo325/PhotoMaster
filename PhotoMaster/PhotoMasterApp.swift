9ed88b72ba40905235138b867d9536f5da01c884
aiueo <uaoi@icloud.com>
2026-02-09 02:01:49 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by Aoi Ueno on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
